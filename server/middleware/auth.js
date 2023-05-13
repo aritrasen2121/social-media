36bb092dad0efb6da7f18a6d7272678c20ebb0a3
aritrasen2121 <aritrasen2121@gmail.com>
2023-05-13 22:14:49 +0530

    unnecery file deleted

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -12,7 +12,9 @@ export const verifyToken = async (req, res, next) => {
       token = token.slice(7, token.length).trimLeft();
     }
 
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    const verified = jwt.verify(token, process.env.JWT_SECRET,{
+      expiresIn: '30s'
+    });
     req.user = verified;
     next();
   } catch (err) {
